6073fdd665245c38e83378ef202b59fdcedba09a
AhmedI968 <86612161+AhmedI968@users.noreply.github.com>
2024-07-30 16:13:52 -0600

    Finished most of the functionality of the code

--- scirpt.js
+++ scirpt.js
@@ -5,30 +5,49 @@ const colorPicker = document.querySelector('#colorPicker');
 const clearButton = document.querySelector('#clear');
 
 const createGraph = (gridSize) => {
+    // Create grid
     gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    // Create grid boxes
     for (let i = 0; i < gridSize * gridSize; i++) {
         const gridBox = document.createElement('div');
         gridBox.classList.add('gridBox');
         gridContainer.appendChild(gridBox);
     };
-    document.querySelectorAll('.dimensions').textContent = `${gridSize}x${gridSize}`;
+    // Display the grid size
+    document.querySelector('.dimensions').textContent = `${gridSize}x${gridSize}`;
 };
 
+// Functionality for each grid
 const addFunctionality = () => {
     let gridBoxList = document.querySelectorAll('.gridBox');
     gridBoxList.forEach(gridBox => {
-        gridBox.addEventListener('mouseover', function(){
+        gridBox.addEventListener('mousedown', function() {
+            isDrawing = true; // Start drawing when mouse button is pressed
             gridBox.style.backgroundColor = colorPicker.value;
         });
+
+        gridBox.addEventListener('mouseup', function() {
+            isDrawing = false; // Stop drawing when mouse button is released
+        });
+
+        gridBox.addEventListener('mouseover', function() {
+            if (isDrawing) { // Only sketch if mouse button is pressed
+                gridBox.style.backgroundColor = colorPicker.value;
+            }
+        });
+
     });
 };
 
+// Update the color of the grid
 colorPicker.addEventListener('change', function(){
     color = colorPicker.value;
 })
 
+// Add functionality to the reset button
 resetButton.addEventListener('click', function(){
+    // Only take new grid size if valud
     let gridSize = prompt('Enter grid size, it should be a value from 1 to 100: ');
     if (gridSize === null) {
         return;
@@ -36,19 +55,24 @@ resetButton.addEventListener('click', function(){
     while (gridSize < 1 || gridSize > 100) {
         gridSize = prompt('Invalid input, please enter a value from 1 to 100');
     }
+    // Clear the grid
     while (gridContainer.firstChild) {
         gridContainer.removeChild(gridContainer.firstChild);
     }
+    // Create new grid
     createGraph(gridSize);
     addFunctionality();
 });
 
+// Add functionality to the clear button
 clearButton.addEventListener('click', function(){
+    // Change color of all grid box to white
     document.querySelectorAll('.gridBox').forEach(gridBox => {
         gridBox.style.backgroundColor = 'white';
     });
 });
 
+// Initialize the grid
 let gridSize = 16;
 createGraph(gridSize);
 addFunctionality();
