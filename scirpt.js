5d001d0560684226805e85985c71ab142193dbe7
AhmedI968 <86612161+AhmedI968@users.noreply.github.com>
2024-07-30 14:39:01 -0600

    Added Functionality  to the select color and the reset button, and began working on the createGraph button

--- scirpt.js
+++ scirpt.js
@@ -1,2 +1,20 @@
 const container = document.querySelector('#container');
 const grid = document.querySelector('#grid');
+const resetButton = document.querySelector('#reset');
+const colorPicker = document.querySelector('#colorPicker');
+
+let gridSize = 16;
+let color = colorPicker.value;
+
+const createGraph = (gridSize) => {
+    
+}
+
+colorPicker.addEventListener('change', function(){
+    color = colorPicker.value;
+})
+
+resetButton.addEventListener('click', function(){
+    gridSize = prompt('Enter grid size, it should be a value from 1 to 100: ');
+    createGraph(gridSize);
+});
